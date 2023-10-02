dae8bab8cc588e00e28775fb06cda1ae151d10e1
yashPundhir <yash.pundhir.prof@gmail.com>
2023-10-02 16:34:06 +0530

    Entire App structure implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import "./App.css";
+import AdditionalQues from "./components/AdditionalQues";
+import Header from "./components/Header";
+import ImageUpload from "./components/ImageUpload";
+import PersonalInfo from "./components/PersonalInfo";
+import Profile from "./components/Profile";
+import Sidebar from "./components/Sidebar";
 
 function App() {
 	return (
-		<div>
-			<h1 className="text-red-600 bg-gray-900 px-12 py-8 rounded-xl">
-				Hello world
-			</h1>
+		<div className="flex flex-row justify-start gap-5">
+			<div className="h-screen fixed left-0 top-0">
+				<Sidebar />
+			</div>
+			<div className="flex flex-col gap-8 mt-4 ml-[80px] pb-14   overflow-y-auto">
+				<Header />
+				<ImageUpload />
+				<PersonalInfo />
+				<Profile />
+				<AdditionalQues />
+			</div>
 		</div>
 	);
 }
